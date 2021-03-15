505de038ffd9ac724569f62c144689e1dbbcf8a6
Pablo Mestanza <pablo93ml@gmail.com>
2021-03-15 23:10:44 +0100

    fix: customer item rename

--- src/components/CustomerItem.tsx
+++ src/components/CustomerItem.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { IonIcon, IonItem, IonLabel } from "@ionic/react";
 import { personOutline } from "ionicons/icons";
 
 const ItemCustomer: React.FC = () => {
+
+  let data = {
+    name: 'Antonio',
+    lastname: 'Fernández Luque',
+    address: 'C/ Avenida Gerald Brenan',
+    date: '12 marzo 2021'
+  };
+
   return (
     <IonItem>
       <IonIcon icon={personOutline} slot="start"></IonIcon>
       <IonLabel>
-        <h2>Antonio Fernández Luque</h2>
-        <h3>C/ Avenida Gerald Brenan</h3>
+        <h2>{data.name} {data.lastname}</h2>
+        <h3>{data.address}</h3>
       </IonLabel>
       <IonLabel position="fixed">
-        <p>12 marzo 2021</p>
+        <p>{data.date}</p>
       </IonLabel>
     </IonItem>
   );
--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IonContent,
   IonList,
   IonSearchbar,
 } from "@ionic/react";
-import ItemCustomer from "../components/ItemCustomer";
+import CustomerItem from "../components/CustomerItem";
 import Header from "../components/Header";
 
 const Customers: React.FC = () => {
   let title = "Clientes";
 
+  const [searchText] = useState('');
+
+  const customerItem = <CustomerItem></CustomerItem>;
+
+  function setSearchText(text: String) {
+    console.log(text);
+  }
+  
+
   return (
     <IonContent>
       <Header title={title}></Header>
-      <IonSearchbar animated={true} placeholder="Buscar"></IonSearchbar>
+      <IonSearchbar animated={true} placeholder="Buscar" value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
       <IonList>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
-        <ItemCustomer></ItemCustomer>
+        {customerItem}
+        {customerItem}
+        {customerItem}
+        {customerItem}
+        {customerItem}
+        {customerItem}
+        {customerItem}
+        {customerItem}
       </IonList>
     </IonContent>
   );
