a636111f8c8f5ef767ba635a07c84a5253ca6a02
mestanza-93 <pablo93ml@gmail.com>
2021-03-17 13:29:44 +0100

    fix: reorganize constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
-import React from 'react';
-import Menu from './pages/Menu';
-import Router from './Router';
+import "./theme/variables.css";
+import React from "react";
+import Menu from "./pages/Menu";
+import Router from "./config/Router";
+import constants from "./constants";
 
-import {
-  IonApp
-} from "@ionic/react";
-
+import { IonApp } from "@ionic/react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const App: React.FC = () => {
 
+  const client = new ApolloClient({
+    uri: constants.API_URL_LOCAL,
+    cache: new InMemoryCache(),
+  });
+
   return (
     <IonApp>
-      <Router></Router>
-      <Menu></Menu>
+      <ApolloProvider client={client}>
+        <Router></Router>
+        <Menu></Menu>
+      </ApolloProvider>
     </IonApp>
   );
 };
--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { gql, useQuery } from '@apollo/client';
+
 import {
   IonContent,
   IonList,
@@ -11,13 +13,30 @@ const Customers: React.FC = () => {
   let title = "Clientes";
 
   const [searchText] = useState('');
-
   const customerItem = <CustomerItem></CustomerItem>;
 
   function setSearchText(text: String) {
     console.log(text);
   }
-  
+
+  const GET_CUSTOMERS = gql`
+    {
+      CustomerMany {
+        name
+        lastname
+        phone
+        address
+        town
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_CUSTOMERS);
+
+  console.log("LOADING: " + loading);
+  console.log("ERROR: " + error);
+  console.log("DATA: " + data);
+
 
   return (
     <IonContent>
