a081b5f28ac7afde87874bd474d0b93c312452f5
Pablo Mestanza <pablomestanza@besoccer.com>
2022-03-31 13:57:23 +0200

    fix: upgrade react and kalend version

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,53 +1,51 @@
-import { IonContent } from "@ionic/react";
+import { IonContent, IonItem, IonItemGroup } from "@ionic/react";
 import React from "react";
 import WorksListInterface from "../interfaces/WorksList";
 import CalendarEventInterface from "../interfaces/CalendarEvent";
 import FormatHelper from "../helpers/FormatHelper";
 
-import Kalend, { CalendarView } from 'kalend';
+import Kalend, { CalendarView } from "kalend";
+import "kalend/dist/styles/index.css";
 
 const Calendar: React.FC<WorksListInterface> = (props) => {
   let works = props.works;
   let id = 0;
-  let events = {};
+  let events: CalendarEventInterface[] = [];
 
   if (works) {
-    works.forEach(work => {
-      let date = FormatHelper.FormatDateCalendar(work.date);
+    works.forEach((work) => {
       let event = {} as CalendarEventInterface;
+
+      let eventName = work.customer.name && work.customer.name !== '' ? work.customer.name : (work.name ?? '');
+
       event = {
         id: work._id,
         startAt: work.date,
         endAt: work.date,
-        timezoneStartAt: 'Europe/Madrid',
-        summary: 'Trabajo',
-        color: FormatHelper.GetRandomColor()
+        timezoneStartAt: "Europe/Madrid",
+        summary: eventName,
+        color: FormatHelper.GetRandomColor(),
       };
       id++;
 
-      if (date) {
-        if (!events[date]) {
-          events[date] = [];
-        }
-        events[date].push(event);
-      }
-
+      events.push(event);
     });
   }
 
   return (
-    <IonContent>
-      <Kalend
-        onEventClick={() => {}}
-        onNewEventClick={() => {}}
-        events={events}
-        initialDate={new Date().toISOString()}
-        hourHeight={30}
-        initialView={CalendarView.MONTH}
-        disabledViews={[CalendarView.DAY]}
-        onPageChange={() => {}}
-      />
-    </IonContent>
+    <Kalend
+      onEventClick={() => {}}
+      onNewEventClick={() => {}}
+      events={events}
+      initialDate={new Date().toISOString()}
+      hourHeight={60}
+      initialView={CalendarView.MONTH}
+      disabledViews={[CalendarView.DAY]}
+      onPageChange={() => {}}
+      timeFormat={"24"}
+      weekDayStart={"Monday"}
+      language={"es"}
+    />
   );
 };
 
