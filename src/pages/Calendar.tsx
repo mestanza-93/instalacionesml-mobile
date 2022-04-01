914a86cae59e51e8c655049b9032a2878cd166b2
Pablo Mestanza <pablomestanza@besoccer.com>
2022-04-01 11:09:40 +0200

    fix: update kalend version

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -13,32 +13,32 @@ const Calendar: React.FC = () => {
    */
   let header = {} as HeaderInterface;
   header.title = "Calendario";
-  
+
   let data = {} as WorksListInterface;
 
   /**
    * Works by date
    */
-   let filters = {} as FilterInterface;
-   filters.sort = "DATE_DESC";
-   filters.limit = 35;
- 
-   const [works, setWorks] = useState(data);
-   data = WorkModel.GetWorksByDate(filters) ?? {};
- 
-   /**
-    * Initialize list data
-    */
-   if (Object.keys(data).length > 0 && Object.keys(works).length === 0) {
-     setWorks(data);
-   }
- 
-   return (
-     <IonContent>
-       <Header {...header}></Header>
-       <CalendarComponent {...works}></CalendarComponent>
-     </IonContent>
-   );
- };
+  let filters = {} as FilterInterface;
+  filters.sort = "DATE_DESC";
+  filters.limit = 35;
+
+  const [works, setWorks] = useState(data);
+  data = WorkModel.GetWorksByDate(filters) ?? {};
+
+  /**
+   * Initialize list data
+   */
+  if (Object.keys(data).length > 0 && Object.keys(works).length === 0) {
+    setWorks(data);
+  }
+
+  return (
+    <IonContent>
+      <Header {...header}></Header>
+      <CalendarComponent {...works}></CalendarComponent>
+    </IonContent>
+  );
+};
 
 export default Calendar;
