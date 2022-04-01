eb916f013f8c9d7a35adfda947f2f1d131fce661
Pablo Mestanza <pablomestanza@besoccer.com>
2022-04-01 14:08:02 +0200

    test: try call new request on calendar page change

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,16 +1,23 @@
 import { IonContent, IonItem, IonItemGroup } from "@ionic/react";
-import React from "react";
+import React, { useState } from "react";
 import WorksListInterface from "../interfaces/WorksList";
 import CalendarEventInterface from "../interfaces/CalendarEvent";
 import FormatHelper from "../helpers/FormatHelper";
-
-import Kalend, { CalendarEvent, CalendarView, CALENDAR_VIEW } from "kalend";
-import "kalend/dist/styles/index.css";
+import WorkModel from "../models/Work";
 import UrlHelper from "../helpers/UrlHelper";
+import FilterInterface from "../interfaces/Filters";
+import { useMutation } from "@apollo/client";
+
+import Kalend, { CalendarEvent, CalendarView, OnPageChangeData } from "kalend";
+import "kalend/dist/styles/index.css";
+
 
 const Calendar: React.FC<WorksListInterface> = (props) => {
-  let works = props.works;
-  let id = 0;
+
+  let worksProps = {} as WorksListInterface;
+  worksProps = props;
+  const [works, setWorks] = useState(worksProps.works);
+
   let events: CalendarEventInterface[] = [];
 
   if (works) {
@@ -30,6 +37,18 @@ const Calendar: React.FC<WorksListInterface> = (props) => {
     });
   }
 
+  const changeDataPage = (changeEvent: OnPageChangeData) => {
+    console.log(changeEvent);
+
+    let filters = {} as FilterInterface;
+    filters.sort = "DATE_DESC";
+    filters.limit = 50;
+  
+    let newDate = WorkModel.GetWorksByDate(filters) ?? {};
+    setWorks(newDate.works);
+
+  };
+
   return (
     <Kalend
       onEventClick={(event: CalendarEvent) => {
@@ -41,7 +60,9 @@ const Calendar: React.FC<WorksListInterface> = (props) => {
       hourHeight={60}
       initialView={CalendarView.MONTH}
       disabledViews={[CalendarView.DAY, CalendarView.WEEK, CalendarView.THREE_DAYS]}
-      onPageChange={() => {}}
+      onPageChange={(changeEvent: OnPageChangeData) => {
+        changeDataPage(changeEvent);
+      }}
       timeFormat={"24"}
       weekDayStart={"Monday"}
       language={"es"}
